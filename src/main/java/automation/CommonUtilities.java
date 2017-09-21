8dba41fcf1cd25c64b64cbe5b2b6fa016fa253f0
Ishan Sood <ishansood.uiit@gmail.com>
2017-09-21 23:58:05 +0530

    use csv as data provider

--- src/main/java/automation/CommonUtilities.java
+++ src/main/java/automation/CommonUtilities.java
@@ -2,7 +2,11 @@ package automation;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.opencsv.CSVReader;
+
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 
@@ -10,23 +14,43 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.text.SimpleDateFormat;
 
 public class CommonUtilities {
     public WebDriver driver;
+    public static List<String []> csvDataList;
+    String fs = System.getProperty("file.separator");   
     public WebDriver initializeDriver() {
     	driver = new FirefoxDriver();
-    	driver.get("https://orangehrm-demo-6x.orangehrmlive.com/");
+    	driver.get("https://orangehrm-demo-6x.orangehrmlive.com/auth/login");
     	driver.manage().window().maximize();
     	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         return driver;
     }
     public void getScreenshot() throws IOException {
     	File scrnFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-    	String fs = System.getProperty("file.separator");
+    	
     	String timeStamp = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
     	String filePath = System.getProperty("user.dir") + fs +  "screenshots" + fs + timeStamp +".png";
     	FileUtils.copyFile(scrnFile, new File(filePath));
+    }
+    
+    public  static List<String []> DataProviderCSV(String path) throws IOException{
+		CSVReader csvreader = new CSVReader(new FileReader(path));
+		csvDataList = csvreader.readAll();
+		csvreader.close();
+		return csvDataList;    	
+    }
+    public static int getCSVRowCount () {
+    	return csvDataList.size();
+    }
+    public static int getCSVColCount() {
+    	return csvDataList.get(0).length;
+    }
+    public static String getCSVData(int row, int col) {
+    	String[] line = csvDataList.get(row);
+    	return line[col];
     }
 }
--- src/test/java/tests/TestsLoginPage.java
+++ src/test/java/tests/TestsLoginPage.java
@@ -27,33 +27,46 @@ public class TestsLoginPage extends CommonUtilities {
 		driver.close();
 	}
 	
-	@Test(dataProvider="getData")
+	@Test(dataProvider="loginData")
 	public void doTesting(String username, String password, String ExpectedResult) throws IOException {
+		System.out.println("n:"+ username+" p:"+password+ " e:"+ExpectedResult);
 		obj_pageLogin.loginToOrangeHRM(username, password);
 		obj_pageHome = new HomePage(driver);
     	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
     	String accountName = obj_pageHome.getAccountName();
     	System.out.println("Account name:"+accountName);
-    	Assert.assertTrue(accountName.equals(ExpectedResult));
     	getScreenshot();
+    	Assert.assertTrue(accountName.equals(ExpectedResult));
 	}
 	
-	@DataProvider
-	public Object[][] getData() {
-		Object[][]  data = new Object[3][3];
-		
-		data[0][0] = "admin";
-		data[0][1] = "admin";
-		data[0][2] = "Jacqueline White";
-		
-		data[1][0] = "linda.anderson";
-		data[1][1] = "linda.anderson";
-		data[1][2] = "Linda Anderson";
-		
-		data[2][0] = "kevin.mathews";
-		data[2][1] = "kevin.mathews";
-		data[2][2] = "Kevin Mathews";
+	@DataProvider(name = "loginData")
+	public Object[][] getData() throws IOException {
+		String fs = System.getProperty("file.separator");
+		String csvFilePath = System.getProperty("user.dir") + fs + "testDataDir" + fs + "loginData.csv";
+		DataProviderCSV(csvFilePath);
+		int rows = getCSVRowCount();
+		int cols = getCSVRowCount();
+		Object[][] data = new Object[rows][cols];
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				data[i][j] = CommonUtilities.getCSVData(i, j);
+			}
+		}
+//		Object[][]  data = new Object[3][3];
+//		
+//		data[0][0] = "admin";
+//		data[0][1] = "admin";
+//		data[0][2] = "Jacqueline White";
+//		
+//		data[1][0] = "linda.anderson";
+//		data[1][1] = "linda.anderson";
+//		data[1][2] = "Linda Anderson";
+//		
+//		data[2][0] = "kevin.mathews";
+//		data[2][1] = "kevin.mathews";
+//		data[2][2] = "Kevin Mathews";
 		
 		return data;
 	}
